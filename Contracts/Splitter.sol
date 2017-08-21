4780230ae16676a7c52b13acbe4b9ccccfb91a49
Karen <kscarbrough1@gmail.com>
2017-08-20 23:08:17 -0500

    Update Splitter.sol

--- Contracts/Splitter.sol
+++ Contracts/Splitter.sol
@@ -55,26 +55,31 @@ function deposit(address recipient1, address recipient2)
 
 //Sending funds to Bob or Carol. Only the action. 
 function withdraw()
-    public
+    public 
     returns (bool success)
 {
-    var bob = msg.sender; 
     //Assuming no need to make this payable as funds paid through deposit()
     //Cases to throw - how to make sure only Bob or Carol can withdraw funds?
     //Use the balances identified above to send money. This should make sure that 
     //only those with identified balances can be sent their balances
-    bob.send(balances[msg.sender]);
-    //What if Bob comes back and tries to steal Carol's funds before she gets them? 
-    //Because balances would still show what Bob is due...
-    //Subtract from contract balance -- transfer should throw if it doesn't work
-    //so balances of contract wouldn't get affected if attacked...right?
-    balances[this] -= balances[msg.sender];
-    //Reset sender balance to zero
-    balances[msg.sender] == 0; 
-    return true;
+    //Bob is paying gas when withdrawing - can contract send gas to another funciton so he doesn't have to pay?
+    if (balances[msg.sender] <=0 ) throw;
+    msg.sender.call.gas(3000000)(secondWithdraw()); 
+    return true; 
 }
 
-        
+function secondWithdraw()
+    private
+    returns (bool success)
+{
+    if (balances[msg.sender] <= 0) throw; 
+    if (!msg.sender.send(balances[msg.sender])) throw;
+    //What if Bob comes back and tries to steal Carol's funds before she gets them? 
+    balances[msg.sender] -= balances[msg.sender];
+    return true;
+}
+   
+    
 //Self destructing kill switch, only works for contract creator
 // Are you able to test selfdestruct() in browser solidity? 
 function stopIt()
@@ -87,73 +92,4 @@ function stopIt()
     }
 }
 
-contract Splitter{
-    address public owner; //"Alice"
-    //Mapping each address to their balance. Only need to
-    //know balances by account
-    mapping (address => uint) public balances; 
 
-
-//Constructor - run once when contract deployed 
-function Splitter()
-//Not payable to avoid confusion of sending Ether during deployment. 
-{
-    //Identifying owner
-    owner = msg.sender;
-}
-
-
-//Takes in the amount to send to Bob & Carol
-function deposit(address recipient1, address recipient2)
-    public
-    payable
-    returns(bool success)
-{
-        //Cases to stop early (throw)
-        if (msg.sender != owner) throw; //if function not sent by owner
-        if (msg.value < 0) throw; 
-        //Base unit for contracts is wei, if division of odd number, remainder 
-        //stays with the contract. Best to throw contract if there is an odd value
-        //with a remainder...(?)
-        if (msg.value % 2 !=0) throw; 
-        //Mapping balances - showing balance of contract
-        balances[this] += msg.value; 
-        //Go ahead and update balance of recipients since money is already here (?)
-        balances[recipient1] += msg.value/2; 
-        balances[recipient2] += msg.value/2; 
-        return true;
-}
-
-//Sending funds to Bob or Carol. Only the action. 
-function withdraw()
-    public
-    returns (bool success)
-{
-    var bob = msg.sender; 
-    //Assuming no need to make this payable as funds paid through deposit()
-    //Cases to throw - how to make sure only Bob or Carol can withdraw funds?
-    //Use the balances identified above to send money. This should make sure that 
-    //only those with identified balances can be sent their balances
-    bob.send(balances[msg.sender]);
-    //What if Bob comes back and tries to steal Carol's funds before she gets them? 
-    //Because balances would still show what Bob is due...
-    //Subtract from contract balance -- transfer should throw if it doesn't work
-    //so balances of contract wouldn't get affected if attacked...right?
-    balances[this] -= balances[msg.sender];
-    //Reset sender balance to zero
-    balances[msg.sender] == 0; 
-    return true;
-}
-
-        
-//Self destructing kill switch, only works for contract creator
-// Are you able to test selfdestruct() in browser solidity? 
-function stopIt()
-    public 
-    returns (bool success)
-    {
-    if (msg.sender != owner) throw; //extra check, just in case
-    selfdestruct(owner); //send all funds back to owner
-    return true;
-    }
-}
