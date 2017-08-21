eb2e4d90f38339d9b983f491c785c3c7fcace504
Karen <kscarbrough1@gmail.com>
2017-08-20 23:04:54 -0500

    Update Splitter.js

--- Test/Splitter.js
+++ Test/Splitter.js
@@ -13,10 +13,12 @@ contract('Splitter', function(accounts){
   var owner =  accounts[0]; //Alice
   var bob = accounts[1]; //Bob
   var carol = accounts[2]; //Carol
-  //Amounts sent to contract
-  var amount1 = 10; //Test an even number
-  var amount2 = 9; //Test an odd number
+   //Amounts sent to contract
+  var amount1 = web3.toWei(10, "ether"); //Test an even number
+  var amount2 = web3.toWei(9, "ether"); //Test an odd number
   var amount3 = 0; //Test zero 
+
+ 
 
   //Steps to take before each test runs, deploy contract each time to 
   //start at "same base case"
@@ -38,39 +40,39 @@ contract('Splitter', function(accounts){
 
   //First test, contract should take in Alice's even deposits
   it("Should take Alice's even deposits.", function(){
-    var alicesDeposit; 
-    alicesCurBalance = web3.eth.getBalance(accounts[0]);
-    //I don't know whether to use the mapped balances or the actual
-    //account balances here
-    return contract.deposit({from:owner, value: amount1}, bob, carol)
+    var alicesDeposit;
+    var contractAddress; 
+    return contract.deposit(bob, carol, {from:owner, gas: 3000000, value: amount1})
     .then(function(txn){
-      return contract.balances({from:owner})
-      .then(function(_balances){
-        alicesDeposit = _balances; 
-        assert.equal(balances[bob], amount/2, "Even amount not received.")
+      contractAddress = contract.address; 
+      alicesDeposit = web3.eth.getBalance(contractAddress);
+      assert.equal(alicesDeposit.toString(10), amount1, "Even amount not received.");
       });
-    //Originally wanted to compare the balance of the contract to what was sent
-    //from Alice - could not find a way to call this here because I didn't know 
-    //how to get the contract address into web3.eth.getBalance()
-    //Trying to see if I can compare Alice's old balance & new balance with the 
-    //difference
-    // var alicesNewBalance;
-    // var theDiff; 
-    // alicesNewBalance = web3.eth.getBalance(accounts[0]);
-    // theDiff = alicesCurBalance-alicesNewBalance; 
-    // //I keep getting "BigNumber" errors...
-    // assert.equal(theDiff.toString(10), amount1.toString(10), "Even amount not received.") 
     });
-  });
 
+  //Second test, contract should give funds to Bob when he calls it
+  it("Should give funds to Bob.", function(){
+      var bobsBalNew; 
+      var diff; 
+      var bobsBalOld = web3.eth.getBalance(accounts[1]);
+      return contract.deposit(bob, carol, {from:owner, value:amount1})
+      .then(function(txn){
+      return contract.withdraw({from:bob})
+      .then(function(txn){
+       bobsBalNew = web3.eth.getBalance(bob); 
+       diff = bobsBalNew.minus(bobsBalOld);
+       assert.equal(diff.toString(10), amount1/2, "Bob didn't get correct funds.") 
+        });
+     });
+  });
+ });
   //Second test, contract should throw odd deposits 
   //Third test, contract should throw 0 value deposits
 
-  //Fourth/Fifth test, contract should give funds to Bob or Carol 
-  it("Should give funds to Bob", function (){
-    return contract.withdraw({from:bob});
-    then(function(txn){
+  // //Fourth/Fifth test, contract should give funds to Bob or Carol 
+  // it("Should give funds to Bob", function (){
+  //   return contract.withdraw({from:bob});
+  //   then(function(txn){
       
-    })
-  })
-})
+  //   });
+  // });
